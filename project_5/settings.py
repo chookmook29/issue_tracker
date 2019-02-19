51d9cc794b36d15b9322873b85bfee27a4ffe656
Piotr Chudziak <strangelove44@gmail.com>
2019-02-19 21:59:16 +0000

    add Postgres database

--- project_5/settings.py
+++ project_5/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project_5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project5db',
+        'USER': 'postgres',
+        'PASSWORD': 'breslau',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
