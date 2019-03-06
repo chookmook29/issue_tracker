0a94236ab31e2d66a6b3566c187a3484d420d903
Piotr Chudziak <strangelove44@gmail.com>
2019-03-06 21:31:09 +0000

    add media url and configuration

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 import blog.views
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('blog/', include('blog.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
