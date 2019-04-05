5cbc694ee8d086e9356c5fdbac7d2482ab628005
Piotr Chudziak <strangelove44@gmail.com>
2019-04-05 23:02:37 +0100

    working logging

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
 	return render(request, 'index.html')
@@ -15,4 +16,14 @@ def ticket(request):
 
 
 def login_user(request):
-	return render(request, 'login.html')
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			return redirect('login')
+	else:
+		return render(request, 'login.html')
