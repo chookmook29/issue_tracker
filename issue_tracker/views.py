a982a54a2ff1df5372921c0545f47e013f5a4a4a
Piotr Chudziak <strangelove44@gmail.com>
2019-04-05 22:13:20 +0100

    add login.html

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,12 +1,18 @@
-
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
 	return render(request, 'index.html')
 
+
 def blog(request):
 	return render(request, 'blog.html')
 
+
 def ticket(request):
 	return render(request, 'ticket.html')
+
+
+def login_user(request):
+	return render(request, 'login.html')
