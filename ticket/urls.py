08a176aca676805b4b3de89f4f10c03b70828872
Piotr Chudziak <strangelove44@gmail.com>
2019-04-05 23:36:38 +0100

    add new ticket link when user authenticated

--- ticket/urls.py
+++ ticket/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.tickets, name='tickets'),
+	path('', views.tickets, name='ticket'),
 	path('delete/<ticket_id>', views.delete, name='delete'),
 ]
