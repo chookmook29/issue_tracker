674adf9b9f429789ac2e6665cfee7eadc3dd46c0
Piotr Chudziak <strangelove44@gmail.com>
2019-04-06 12:17:17 +0100

    add feature and bug choices

--- ticket/models.py
+++ ticket/models.py
@@ -3,6 +3,22 @@ from django.db import models
 class Ticket(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
+    TO_DO = 'TD'
+    DOING = 'DG'
+    DONE = 'DN'
+    STATUS_CHOICES = (
+    	(TO_DO, 'To do'),
+    	(DOING, 'Doing'),
+    	(DONE, 'Done'),
+    )
+    BUG = 'BG'
+    FEATURE = 'FT'
+    TYPE_CHOICES = (
+    	(BUG, 'Bug'),
+    	(FEATURE, 'Feature'),
+    )
+    progress = models.CharField(max_length=3, choices=STATUS_CHOICES, default='1')
+    ticket_type = models.CharField(max_length=2, choices=TYPE_CHOICES, default='1')
 
 
 class Comment(models.Model):
