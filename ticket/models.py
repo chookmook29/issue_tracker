7cfb1c60a7a335c7a54905607828bd9a2866d1ea
Piotr Chudziak <strangelove44@gmail.com>
2019-04-27 21:05:38 +0100

    modify tickets display, ticket model settings

--- ticket/models.py
+++ ticket/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
+
 class Ticket(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
-    TO_DO = 'TD'
-    DOING = 'DG'
-    DONE = 'DN'
+    TO_DO = 'To do'
+    DOING = 'Doing'
+    DONE = 'Done'
     STATUS_CHOICES = (
-    	(TO_DO, 'To do'),
-    	(DOING, 'Doing'),
-    	(DONE, 'Done'),
+        (TO_DO, 'To do'),
+        (DOING, 'Doing'),
+        (DONE, 'Done'),
     )
-    BUG = 'BG'
-    FEATURE = 'FT'
+    BUG = 'Bug'
+    FEATURE = 'Feature'
     TYPE_CHOICES = (
-    	(BUG, 'Bug'),
-    	(FEATURE, 'Feature'),
+        (BUG, 'Bug'),
+        (FEATURE, 'Feature'),
     )
-    progress = models.CharField(max_length=3, choices=STATUS_CHOICES, default='1')
-    ticket_type = models.CharField(max_length=2, choices=TYPE_CHOICES, default='1')
+    progress = models.CharField(max_length=10, choices=STATUS_CHOICES, default='1')
+    ticket_type = models.CharField(max_length=10, choices=TYPE_CHOICES, default='1')
 
 
 class Comment(models.Model):
