f75f6c40a6260322252cc002b04dd4c3fdfd1b32
Piotr Chudziak <strangelove44@gmail.com>
2019-04-28 14:17:49 +0100

    add single ticket display

--- ticket/urls.py
+++ ticket/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.tickets, name='ticket'),
-	path('delete/<ticket_id>', views.delete, name='delete'),
+    path('', views.tickets, name='ticket'),
+    path('delete/<ticket_id>', views.delete, name='delete'),
+    path('show/<ticket_id>', views.show, name='show_single'),
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render, redirect
 from .models import Ticket, Comment
 from django.http import HttpResponseRedirect
 
+
 def tickets(request):
-	tickets = Ticket.objects.order_by('title')
-	comments = Comment.objects.get()
-	return render(request, 'ticket/tickets.html', {'tickets': tickets}, {'comments': comments})
+    tickets = Ticket.objects.order_by('title')
+    comments = Comment.objects.get()
+    return render(request, 'ticket/tickets.html', {'tickets': tickets}, {'comments': comments})
+
 
 def delete(request, ticket_id):
-	item = Ticket.objects.get(pk=ticket_id)
-	item.delete()
-	return redirect('/')
+    item = Ticket.objects.get(pk=ticket_id)
+    item.delete()
+    return redirect('/')
+
+
+def show(request, ticket_id):
+    ticket = Ticket.objects.get(pk=ticket_id)
+    return render(request, 'single_ticket.html', {'ticket': ticket})
