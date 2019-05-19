ade5c7bff8f81126f93688049e250342e1321719
Piotr Chudziak <strangelove44@gmail.com>
2019-05-19 21:13:23 +0100

    Change blog model media field path

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,4 @@ class Blog(models.Model):
     title =  models.CharField(max_length=255)
     pub_date =  models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='images/',default='default.jpg')
+    image = models.ImageField(upload_to='',default='default.jpg')
