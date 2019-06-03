9ce59c9a593f9d5f38db5964e0af99546df658e3
Piotr Chudziak <strangelove44@gmail.com>
2019-06-04 00:11:36 +0100

    Simplify templates routing

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,9 @@ from .models import Blog, Blog_comment
 
 def blogs(request):
     all_blogs = Blog.objects.order_by('pub_date')
-    return render(request, 'blog/blogs.html', {'all_blogs': all_blogs})
+    return render(request, 'blogs.html', {'all_blogs': all_blogs})
 
 
 def single_blog(request, pk):
     blog = Blog.objects.get(pk=pk)
-    return render(request, 'blog/single_blog.html', {'blog': blog})
+    return render(request, 'single_blog.html', {'blog': blog})
--- ticket/views.py
+++ ticket/views.py
@@ -22,7 +22,7 @@ def show(request, pk):
     page = request.GET.get('page')
     comments = paginator.get_page(page)
     key = settings.STRIPE_PUBLISHABLE_KEY
-    return render(request, 'ticket/single_ticket.html', {'ticket': ticket, 'comments':
+    return render(request, 'single_ticket.html', {'ticket': ticket, 'comments':
                   comments, 'key': key})
 
 
@@ -38,7 +38,7 @@ def add_comment(request, pk):
             return redirect('show_single', pk=pk)
     else:
         form = CommentForm()
-    return render(request, 'ticket/add_comment.html', {'form': form})
+    return render(request, 'add_comment.html', {'form': form})
 
 
 def add_ticket(request):
@@ -53,4 +53,4 @@ def add_ticket(request):
             return redirect('home')
     else:
         form = TicketForm()
-    return render(request, 'ticket/new_ticket.html', {'form': form})
+    return render(request, 'new_ticket.html', {'form': form})
