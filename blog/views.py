7c5466787bc7ed05be6c26e9730688679eca65c2
Piotr Chudziak <strangelove44@gmail.com>
2019-06-04 10:26:21 +0100

    Add comments visibility to template

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,6 @@ def blogs(request):
 
 def single_blog(request, pk):
     blog = Blog.objects.get(pk=pk)
-    return render(request, 'single_blog.html', {'blog': blog})
+    comment_list = Blog_comment.objects.select_related().filter(
+                   ticket_id=pk).order_by('-date')
+    return render(request, 'single_blog.html', {'blog': blog, 'comment_list': comment_list})
