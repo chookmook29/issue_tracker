e278631bfccb69379400c49369a6ef420ab402ff
Piotr Chudziak <strangelove44@gmail.com>
2019-06-06 10:17:42 +0100

    Add sign up option

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('ticket/', include('ticket.urls')),
     path('checkout/', include('checkout.urls')),
     path('login/', views.login_user, name='login'),
+    path('sign_up', views.sign_up, name='sign_up'),
     path('logout/', views.logout_user, name='logout'),
     path('all_tickets/', views.all_tickets, name='all_tickets')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -21,6 +21,10 @@ def login_user(request):
         return render(request, 'login.html')
 
 
+def sign_up(request):
+    return render(request, 'sign_up.html')
+
+
 def logout_user(request):
     logout(request)
     return redirect('home')
