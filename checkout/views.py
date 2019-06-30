a37d06e3a2894ba95f54bf7ace39e576f0c1ea1d
Piotr Chudziak <strangelove44@gmail.com>
2019-06-30 14:29:36 +0100

    Add upvotes display and working contributions

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from ticket.models import Ticket
+from users.models import CustomUser
 
 
 def checkout(request, pk):
     ticket = Ticket.objects.get(pk=pk)
+    user = CustomUser.objects.get(username=request.user)
+    user.contributions += 5
+    user.save()
+    ticket.upvotes += 1
+    ticket.save()
     return render(request, 'checkout.html', {'ticket': ticket})
