6c10fedcaa581b7bdd20c88af4d5906bf7534e23
Piotr Chudziak <strangelove44@gmail.com>
2019-07-04 09:07:24 +0100

    Add Ticket model test

--- ticket/tests.py
+++ ticket/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from ticket.models import Ticket
 
-# Create your tests here.
+
+class TicketTestCase(TestCase):
+    def setUp(self):
+        Ticket.objects.create(title="test", body="text")
+
+    def test_ticket_fields(self):
+        """Ticket created has its correct default values"""
+        ticket = Ticket.objects.get(title="test")
+        self.assertEqual(ticket.upvotes, '0')
+        self.assertEqual(ticket.type, '1')
+        self.assertEqual(ticket.progress, 'To do')
