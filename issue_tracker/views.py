6a6225fee548edf87371cffde186bf305e675824
Piotr Chudziak <strangelove44@gmail.com>
2019-07-08 21:25:16 +0100

    Add comments

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -7,6 +7,8 @@ from users.models import CustomUser
 
 
 def home(request):
+    """Dataset is used in d3.js wordcloud in index.html template, users number
+       narrowed to five for design reasons"""
     dataset = CustomUser.objects.all().order_by('-amount_comments')[:5]
     return render(request, 'index.html', {'dataset': dataset})
 
@@ -18,6 +20,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            # messages from django.contrib improve UX
             messages.success(request, ('User logged in!'))
             return redirect('home')
         else:
@@ -29,9 +32,11 @@ def login_user(request):
 
 def sign_up(request):
     if request.method == 'POST':
+        # CustomUserCreationForm necessary if CustomUser's being used
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            # cleaned_data attribute necessary for is_valid() method
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
@@ -44,11 +49,13 @@ def sign_up(request):
 
 
 def logout_user(request):
+    # Simple logout using django.contrib.auth module
     logout(request)
     messages.success(request, ('User logged out!'))
     return redirect('home')
 
 
 def all_tickets(request):
+    """ Reverse order to make most upvoted ticket appear on top"""
     all_tickets = Ticket.objects.order_by('upvotes').reverse()
     return render(request, 'all_tickets.html', {'all_tickets': all_tickets})
