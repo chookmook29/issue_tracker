5e11df7b2efcfbcbdbcfd6ac7db0e89ecbd49d5e
Piotr Chudziak <strangelove44@gmail.com>
2019-07-14 11:01:22 +0100

    Add more S3 settings

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -1,7 +1,5 @@
 # Needed for Heroku deployment
 import os
-# Configuration for Heroku deployment, essential/otherwise won't deploy
-import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -136,6 +134,3 @@ MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_S')
 
 STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_P')
-
-# For Django-Heroku activation process, suppose to be at the bottom of the file
-django_heroku.settings(locals())
