f28867131d37db05fe812ee9f4047461a3c8685a
Piotr Chudziak <strangelove44@gmail.com>
2019-08-01 23:37:42 +0100

    Add user model test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import CustomUser
 
-# Create your tests here.
+
+class TicketTestCase(TestCase):
+    def setUp(self):
+        CustomUser.objects.create(username="user", password="password")
+
+    def test_ticket_fields(self):
+        """CustomUser created has its correct default values"""
+        user = CustomUser.objects.get(username="user")
+        self.assertEqual(user.is_superuser, False)
+        self.assertEqual(user.is_staff, False)
+        self.assertEqual(user.is_active, True)
+        self.assertEqual(user.contributions, 0)
+        self.assertEqual(user.amount_comments, 0)
+        self.assertEqual(user.last_name, "")
