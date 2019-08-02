a2cfb9aa671b3579f17de7d08379b29a9b927116
chookmook29 <strangelove44@gmail.com>
2019-08-02 12:43:27 +0100

    Add blog test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from blog.models import Blog
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+    def setUp(self):
+        Blog.objects.create(title='test', body='text')
+
+    def test_blog_fields(self):
+        """Blog created has its correct default values"""
+        ticket = Ticket.objects.get(title="test")
+        self.assertEqual(ticket.image, 'default.jpg')
+        self.assertEqual(ticket.body, 'text')
+
+    def test_blogview_url_exists_at_desired_location(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
