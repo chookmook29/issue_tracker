35af6900b653c98605678148f1c3c3feccf53927
Piotr Chudziak <strangelove44@gmail.com>
2019-08-14 21:10:37 +0100

    Change naming of file

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -7,7 +7,7 @@ def home(request):
     """Dataset is used in d3.js wordcloud in index.html template, users number
        narrowed to five for design reasons"""
     dataset = CustomUser.objects.all().order_by('-amount_comments')[:5]
-    return render(request, 'index.html', {'dataset': dataset})
+    return render(request, 'charts.html', {'dataset': dataset})
 
 
 def info(request):
