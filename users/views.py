6d3b31dffe0bba2aa8d2ed7d1a1adf39c1577313
Piotr Chudziak <strangelove44@gmail.com>
2019-08-14 22:26:13 +0100

    Set minimum username length

--- users/views.py
+++ users/views.py
@@ -6,62 +6,66 @@ from users.models import CustomUser
 
 
 def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            # messages from django.contrib improve UX
-            messages.success(request, ('User logged in!'))
-            return redirect('home')
-        else:
-            messages.success(request, ('Error please try again!'))
-            return redirect('login')
-    else:
-        return render(request, 'login.html')
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			# messages from django.contrib improve UX
+			messages.success(request, ('User logged in!'))
+			return redirect('home')
+		else:
+			messages.success(request, ('Error please try again!'))
+			return redirect('login')
+	else:
+		return render(request, 'login.html')
 
 
 def sign_up(request):
-    if request.method == 'POST':
-        # CustomUserCreationForm necessary if CustomUser's being used
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # cleaned_data attribute necessary for is_valid() method
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, ('You successfully signed up!'))
-            return redirect('home')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'sign_up.html', {'form': form})
+	if request.method == 'POST':
+		# CustomUserCreationForm necessary if CustomUser's being used
+		form = CustomUserCreationForm(request.POST)
+		if form.is_valid():
+			if len(form.cleaned_data['username']) < 4:
+				messages.success(request, ('Username too short!'))
+				form = CustomUserCreationForm()
+			else:
+				form.save()
+				# cleaned_data attribute necessary for is_valid() method
+				username = form.cleaned_data['username']
+				password = form.cleaned_data['password1']
+				user = authenticate(username=username, password=password)
+				login(request, user)
+				messages.success(request, ('You successfully signed up!'))
+				return redirect('home')
+	else:
+		form = CustomUserCreationForm()
+	return render(request, 'sign_up.html', {'form': form})
 
 
 def logout_user(request):
-    # Simple logout using django.contrib.auth module
-    logout(request)
-    messages.success(request, ('User logged out!'))
-    return redirect('home')
+	# Simple logout using django.contrib.auth module
+	logout(request)
+	messages.success(request, ('User logged out!'))
+	return redirect('home')
 
 
 def change_details(request):
-    if request.method == 'POST':
-        # CustomUserCreationForm necessary if CustomUser's being used
-        form = CustomUserChangeForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            # cleaned_data attribute necessary for is_valid() method
-            email = form.cleaned_data['email']
-            user = authenticate(email=email)
-            login(request, user)
-            messages.success(request,
-                             ('You successfully changed your details!'))
-            return redirect('home')
-    else:
-        form = CustomUserChangeForm(instance=request.user)
-    user_account = CustomUser.objects.get(username=request.user)
-    return render(request, 'change.html',
-                  {'form': form, 'user_account': user_account})
+	if request.method == 'POST':
+		# CustomUserCreationForm necessary if CustomUser's being used
+		form = CustomUserChangeForm(request.POST, instance=request.user)
+		if form.is_valid():
+			form.save()
+			# cleaned_data attribute necessary for is_valid() method
+			email = form.cleaned_data['email']
+			user = authenticate(email=email)
+			login(request, user)
+			messages.success(request,
+							 ('You successfully changed your details!'))
+			return redirect('home')
+	else:
+		form = CustomUserChangeForm(instance=request.user)
+	user_account = CustomUser.objects.get(username=request.user)
+	return render(request, 'change.html',
+				  {'form': form, 'user_account': user_account})
