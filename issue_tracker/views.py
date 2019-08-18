b87d486c62bbee622afd21c3b33b1140347f3b40
Piotr Chudziak <strangelove44@gmail.com>
2019-08-18 23:28:57 +0100

    Change user display options

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -6,7 +6,7 @@ from users.models import CustomUser
 def home(request):
     """Dataset is used in d3.js wordcloud in index.html template, users number
        narrowed to five for design reasons"""
-    dataset = CustomUser.objects.all().order_by('-amount_comments')[:5]
+    dataset = CustomUser.objects.all().order_by('-amount_comments')[:7]
     return render(request, 'charts.html', {'dataset': dataset})
 
 
