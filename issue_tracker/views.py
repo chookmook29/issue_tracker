64c0d43b1c00727ce756d30dab9fbcf526bc8cae
Piotr Chudziak <strangelove44@gmail.com>
2019-08-26 12:52:23 +0100

    Split and customize datasets

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -6,8 +6,10 @@ from users.models import CustomUser
 def home(request):
     """Dataset is used in d3.js wordcloud in index.html template, users number
        narrowed to five for design reasons"""
-    dataset = CustomUser.objects.all().order_by('-amount_comments')[:7]
-    return render(request, 'charts.html', {'dataset': dataset})
+    dataset_cloud = CustomUser.objects.all().order_by('-amount_comments')[:8]
+    # Different size of databases for both charts to improve readability
+    dataset_pie = CustomUser.objects.all().order_by('-amount_comments')[:6]
+    return render(request, 'charts.html', {'dataset_cloud': dataset_cloud, 'dataset_pie': dataset_pie})
 
 
 def info(request):
